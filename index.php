88c09acf9a0f201bdf363516a78675dece093106
seo-alexbir <https://github.com/seo-alexbir/snippets>
2020-01-22 16:21:47 +0300

    Create index.php
    220120

---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->    
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="template_styles.css">
+
+    <!--<link rel="stylesheet" href="css/owl.carousel.min.css"><!-- owl.carousel.min -->
+    <!--<link rel="stylesheet" href="css/owl.themedefault.css"><!-- owl.carousel.min -->
+
+    <title>Главная страница</title>
+</head>
+<body>
+    <header class="" style="background-color: #ccc;">
+		<div class="container" style="background-color: #ddd; height: 50px; max-width:1400px">
+			<div>header</div>
+		</div>
+	</header>
+    
+    <main class="">
+		<div class="container my-3" style="max-width:1400px">
+            <div class="row">
+                <div class="col-2" style="background-color: #ffa811;">Menu</div>
+                <div class="col" style="background-color: #cdb8d2;">
+                    <div class="row pb-4">
+                        <h1 class="col-12 pt-3" style="font-size:26px">H!H1h1h1h1h1h1h1h1h1h1h1h1h</h1>
+                        <div class="col-12">
+                            <span class="col-4">Производитель: Эсперо</span>
+                            <span class="col-4">Артикл: 24654654545</span>
+                            <span class="col-4">Рейтинг-свездочки</span>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-5">
+                            <div class="" style="border: 1px solid #ccc;height: 400px;">img</div>
+                        </div>
+                        <div class="col-7">
+                            <div class="row">
+                                <div class="col-7" style="background-color: #b3d7ff;">text</div>
+                                <div class="col-5" style="background-color: #b8d2c3;">text2</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+		
+	<footer style="background-color: #ccc;">
+        <div class="container" style="background-color: #ddd; height: 150px; max-width:1400px"">
+            footer
+        </div>
+    </footer>
+    
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="/js/jquery-3.4.1.min.js"></script>
+<script src="/js/bootstrap.js"></script>
+
+</body>
+</html>
