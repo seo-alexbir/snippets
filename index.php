87cf467154429fbf87de05604fac8fc9b6a83bc4
seo-alexbir <https://github.com/seo-alexbir/snippets>
2020-01-22 16:37:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -15,46 +15,8 @@
     <title>Главная страница</title>
 </head>
 <body>
-    <header class="" style="background-color: #ccc;">
-		<div class="container" style="background-color: #ddd; height: 50px; max-width:1400px">
-			<div>header</div>
-		</div>
-	</header>
-    
-    <main class="">
-		<div class="container my-3" style="max-width:1400px">
-            <div class="row">
-                <div class="col-2" style="background-color: #ffa811;">Menu</div>
-                <div class="col" style="background-color: #cdb8d2;">
-                    <div class="row pb-4">
-                        <h1 class="col-12 pt-3" style="font-size:26px">H!H1h1h1h1h1h1h1h1h1h1h1h1h</h1>
-                        <div class="col-12">
-                            <span class="col-4">Производитель: Эсперо</span>
-                            <span class="col-4">Артикл: 24654654545</span>
-                            <span class="col-4">Рейтинг-свездочки</span>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-5">
-                            <div class="" style="border: 1px solid #ccc;height: 400px;">img</div>
-                        </div>
-                        <div class="col-7">
-                            <div class="row">
-                                <div class="col-7" style="background-color: #b3d7ff;">text</div>
-                                <div class="col-5" style="background-color: #b8d2c3;">text2</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </main>
-		
-	<footer style="background-color: #ccc;">
-        <div class="container" style="background-color: #ddd; height: 150px; max-width:1400px"">
-            footer
-        </div>
-    </footer>
+
+<p><a href=""></a>
     
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
